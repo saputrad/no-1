44daa76f6ae89302d23fe50f74b80001873b94c2
saputrad <saputrapaskahd@gmail.com>
2016-03-29 09:50:48 +0700

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -9,19 +9,20 @@ public class Fibonacci {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        int a=0;
-        int b=1;
-        int n=Integer.parseInt(JOptionPane.showInputDialog("Input: "));
- 
-        System.out.println("Input: " + n);
- 
-        for(int i=0; i < n; i++){
-            System.out.print("" + a);
-            a=a+b;
-            b=a-b;
-        }
-        System.out.println("");
-    }
- 
+    public static void main(String[]args)
+ {
+  int a = Integer.parseInt(JOptionPane.showInputDialog("Masukkan nilai awal Deret Fibonacci"));
+  int b = a;
+  int c = Integer.parseInt(JOptionPane.showInputDialog("Masukkan deret Fibonacci yang Anda Inginkan: "));
+  int d = c-1;
+  int e = 1;
+  System.out.print(a + " ");
+  while(e <=d)
+  {
+   System.out.print(a +" ");
+   a = a+b;
+   b = a-b;
+   e++;
+  }
+ }
 }
